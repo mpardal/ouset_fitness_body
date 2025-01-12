d53da2ec52fa4d27bb44c4e469072961b04ce836
mpardal@vpauto.eu <mpardal@vpauto.eu>
2025-01-12 22:43:20 +0100

    pass in env

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -2,19 +2,17 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDtdwHmpAt4o1CC5MZM7XYWzrsddr3QhXo",
-    authDomain: "ouest-fitness-body-6a187.firebaseapp.com",
-    projectId: "ouest-fitness-body-6a187",
-    storageBucket: "ouest-fitness-body-6a187.firebasestorage.app",
-    messagingSenderId: "195830334184",
-    appId: "1:195830334184:web:4f1d86c378229667e43f5d"
+    apiKey: process.env.API_KEY_FIREBASE,
+    authDomain: process.env.AUTH_DOMAIN_FIREBASE,
+    projectId: process.env.PROJECT_ID_FIREBASE,
+    storageBucket: process.env.STORAGE_BUCKET_FIREBASE,
+    messagingSenderId: process.env.MESSENGER_SENDER_ID_FIREBASE,
+    appId: process.env.APP_ID_FIREBASE
 };
 
 const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app);
 export const firestore = getFirestore(app);

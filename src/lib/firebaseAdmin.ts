f67c914897c1500a3a4fd5c2fe7d8c4c5811177d
mpardal@vpauto.eu <mpardal@vpauto.eu>
2025-01-13 00:33:26 +0100

    sitemap & robots

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,10 +1,8 @@
 import admin from "firebase-admin";
 
 // Import statique pour le fichier JSON en local
-import serviceAccount from "../../ouest-fitness-body-firebase_admin.json";
 
 if (!admin.apps.length) {
-    if (process.env.NODE_ENV === "production") {
         // Configuration pour l'environnement de production
         admin.initializeApp({
             credential: admin.credential.cert({
@@ -13,12 +11,6 @@ if (!admin.apps.length) {
                 privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
             }),
         });
-    } else {
-        // Configuration pour l'environnement local
-        admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount as admin.ServiceAccount), // Typé pour TypeScript
-        });
-    }
 }
 
 export const adminFirestore = admin.firestore();

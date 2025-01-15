b1626d86c71ae31ea3c716fe6ab099fce9a2b23f
mpardal@vpauto.eu <mpardal@vpauto.eu>
2025-01-15 12:54:09 +0100

    images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import {IMAGES} from "@/constants/images";
 const Home = () => {
   return (
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-3xl font-bold py-4 text-black">Bienvenue chez Ouest Fitness Body</h1>
+        <h1 className="text-3xl font-bold py-4 text-black text-center">Bienvenue chez Ouest Fitness Body</h1>
           <Image
               src={IMAGES.logo}
               alt="OFB"
--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,9 +1,9 @@
 export const IMAGES = {
-    logo: `${process.env.NEXT_PUBLIC_IMAGE_BASE_URL_LOGO}`,
-    bras_de_fer: `${process.env.NEXT_PUBLIC_IMAGE_URL_BRAS}`,
-    hip_trust: `${process.env.NEXT_PUBLIC_IMAGE_URL_TRUST}`,
-    souleve_de_terre: `${process.env.NEXT_PUBLIC_IMAGE_URL_SOULEVE}`,
-    squat: `${process.env.NEXT_PUBLIC_IMAGE_URL_SQUAT}`,
-    tire_a_la_corde: `${process.env.NEXT_PUBLIC_IMAGE_URL_CORDE}`,
-    traction: `${process.env.NEXT_PUBLIC_IMAGE_URL_TRACTION}`,
+    logo: "https://firebasestorage.googleapis.com/v0/b/ouest-fitness-body-6a187.firebasestorage.app/o/banniere-OFB.JPG?alt=media&token=c3de069f-6fc9-4016-a0b1-fcd5aeb891e5",
+    bras_de_fer: "https://firebasestorage.googleapis.com/v0/b/ouest-fitness-body-6a187.firebasestorage.app/o/bras_de_fer.jpg?alt=media&token=4d286276-0888-4377-a826-eeea8c33e9c4",
+    hip_trust: "https://firebasestorage.googleapis.com/v0/b/ouest-fitness-body-6a187.firebasestorage.app/o/hip-thrust.jpg?alt=media&token=38b78d2f-7c38-4a57-8d24-c1cf34f20408",
+    souleve_de_terre: "https://firebasestorage.googleapis.com/v0/b/ouest-fitness-body-6a187.firebasestorage.app/o/souleve%CC%81-de-terre.avif?alt=media&token=a0dec580-835f-4ae2-b63e-60a206230929",
+    squat: "https://firebasestorage.googleapis.com/v0/b/ouest-fitness-body-6a187.firebasestorage.app/o/squat.jpg?alt=media&token=7f5f959e-6f14-4ae3-8166-8629a6abc3f8",
+    tire_a_la_corde: "https://firebasestorage.googleapis.com/v0/b/ouest-fitness-body-6a187.firebasestorage.app/o/tir_a_la_corde.jpg?alt=media&token=0a288dea-6764-482a-932b-5bc3196bf0ec",
+    traction: "https://firebasestorage.googleapis.com/v0/b/ouest-fitness-body-6a187.firebasestorage.app/o/tractions.avif?alt=media&token=188d1284-f44a-498a-bde5-ebf4ae40ee34",
 };
